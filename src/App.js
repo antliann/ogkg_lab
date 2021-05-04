235e48c5022866eb2126e70444106001a34158c2
antliann <antonliann@gmail.com>
2021-05-04 21:09:53 +0300

    manual input insertion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, {useState} from 'react';
 import './index.css';
 import {Form} from './Form';
-import {Field} from './Field';
+import {Field} from "./Field";
 
 function App() {
     const [points, setPoints] = useState([]);
 
     return (
         <div className="App">
-            <Form setPoints={(pointsForSet) => setPoints(pointsForSet)} />
+            <Form setPoints={(pointsForSet) => setPoints([...pointsForSet])} />
             <button className="main-btn">
                 Build convex hull and inscribe triangle
             </button>
 
-            <Field points={points}/>
+            <Field points={points} />
 
             <div className="num num100y">100</div>
             <div className="num num0">0</div>
